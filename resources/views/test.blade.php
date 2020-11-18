24d7ca4713dd859d5ad4eb8c0bc19d52f7eb73c4
Toby Twigger <tobytwigger1@gmail.com>
2020-11-18 13:03:10 +0000

    Updates

---
+++ resources/views/test.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Test</title>
+
+
+</head>
+<body>
+<div class="container">
+    <form action="/test" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-3">
+                <x-su-select :items="$themes"></x-su-select>
+            </div>
+            <div class="col-3">
+                <x-su-button type="success">Choose Theme</x-su-button>
+            </div>
+        </div>
+    </form>
+
+</div>
+
+@include('theme::assets')
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/theme/{theme}/{group}/{component}', [\App\Http\Controllers\DocumentationController::class, 'component'])
     ->name('component');
+
+Route::get('/test', [\App\Http\Controllers\TestController::class, 'index']);
+Route::post('/test', [\App\Http\Controllers\TestController::class, 'change']);
